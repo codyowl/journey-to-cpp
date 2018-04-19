7ad04afaad2b5a6961d462faf285812517253c04
codyowl <codyowl@gmail.com>
2018-04-19 17:33:44 +0530

    Multi dimensional added :heavy_plus_sign:

--- Datastructures/array.cpp
+++ Datastructures/array.cpp
@@ -18,6 +18,20 @@ int main()
 	
 	return 0;
 
+	//Multidimensional array 
+	//two dimenstional array
+	//syntax : datatype arrayname[row][column]
+
+	int numbers[2][3] = {
+		//first row
+		{2, 3, 4},
+		{4, 5, 6},
+		{7, 8, 9},
+	};
+
+	//Accessing two dimensional array by index
+	cout <<"The second row , third column value is :" << numbers[2][3] << endl;
+
 }	
 
 	
---
+++ Datastructures/multi_dimensional_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+
+{
+
+	// Two dimenstional array 
+	/* syntax datatype[row][column] = {
+	{first sequence},
+    {n sequence} */
+	
+	int twodimensionalarray[2][3] = {
+		{1,2},
+		{1,2,3}
+	};
+
+	cout <<"The first row second value is : " << twodimensionalarray[1][2] << endl;
+
+}	
+
+	
