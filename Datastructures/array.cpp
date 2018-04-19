1a54689dbf48419b533a166acacbb90306184b00
codyowl <codyowl@gmail.com>
2018-04-19 16:28:01 +0530

    array added :heavy_plus_sign:

---
+++ Datastructures/array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+
+{
+
+	//Assigning values to array
+	//syntax datatype arrayname[size] = {sequence of elements}
+
+	int numbers[5] = {1,2,3,4,5};
+
+	//Access by index
+	cout <<"The first value of numbers is : "<< numbers[0] << endl;
+	
+	return 0;
+
+}	
+
+	
+
