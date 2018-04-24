08caeca5a9b7b04c4ccb38b9a7426746a70220df
codyowl <codyowl@gmail.com>
2018-04-24 15:28:35 +0530

    Introduction to while loop :heavy_plus_sign:

---
+++ Loops/whileloop_introduction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 10;
+
+	/* while loop syntax:
+	 * while (condition) 
+	 {
+		 logic;
+     }
+     The while loop will be alive till the condiction becomes false
+    */ 
+
+	while (a < 20)
+	{
+		cout << "The value of a : " << a << endl;
+        a++;  
+	}
+}
+	
