fef45eaca34a48e87854427524b68318de6ccce0
codyowl <codyowl@gmail.com>
2018-05-23 22:06:14 +0530

    Introduction to array added to compound types :rocket:

---
+++ compoundtypes/array_introduction.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+//array declaration 
+//syntax datatype ARRAYNAME[ARRAYSIZE]
+
+int marks[10];
+
+//main function 
+int main() {
+    //assigning values to the array
+    marks[0] = 35;
+    marks[1] = 45;
+    marks[2] = 55;
+    marks[3] = 65;
+    marks[4] = 75;
+    marks[5] = 85;
+    marks[6] = 95;
+    marks[7] = 100;
+    marks[8] = 120;
+    marks[9] = 140;
+    
+    cout << "The first element in the array :" << marks[0] << endl;
+    cout << "The second element in the array: " << marks[1] << endl;
+    cout << "The third element in the array: " << marks[2] << endl;
+    
+    //complete element 
+    cout << "All the elements in the array: " << marks[] << endl;
+    
+}
